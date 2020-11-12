d863470d58da971a28913af0719019aac96a79f2
Aki L <n9loak00@students.oamk.fi>
2020-11-12 19:44:37 +0200

    karuselli ja perusta. ie. footer, navi ja värejä

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -14,7 +14,7 @@
     <title>Cippoi & Cuppei</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
       <a class="navbar-brand logo" href="#">Cippoi & Cuppei</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
