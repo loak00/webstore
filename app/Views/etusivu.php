1cd57c48d43804b5491437997318476e73e61913
Aki L <n9loak00@students.oamk.fi>
2020-11-17 19:03:31 +0200

    Pienempiä kuvia

--- app/Views/etusivu.php
+++ app/Views/etusivu.php
@@ -17,13 +17,13 @@
                 </ol>
                 <div class="carousel-inner" role="listbox">
                     <div class="carousel-item active">
-                        <img class="d-block img-fluid" src="<?= base_url('img/promo1.jpg')?>" alt="First slide">
+                        <img class="d-block img-fluid" src="<?= base_url('img/promo1sm.jpg')?>" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block img-fluid" src="<?= base_url('img/promo2.jpg')?>" alt="Second slide">
+                        <img class="d-block img-fluid" src="<?= base_url('img/promo2sm.jpg')?>" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block img-fluid" src="<?= base_url('img/promo3.jpg')?>" alt="Third slide">
+                        <img class="d-block img-fluid" src="<?= base_url('img/promo3sm.jpg')?>" alt="Third slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
