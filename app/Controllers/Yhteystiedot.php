f710482c65879e279402021127dcbb08f24f02c8
n9leja02 <n9leja01@students.oamk.fi>
2020-11-18 20:14:18 +0200

    lisatty LoginModel yhteystiedot Controlleriin istuntomuuttajan tilalle

--- app/Controllers/Yhteystiedot.php
+++ app/Controllers/Yhteystiedot.php
@@ -1,24 +1,27 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 use App\Models\TuoteryhmaModel;
+use App\Models\LoginModel;
 
 
 class Yhteystiedot extends BaseController
 {
-  private $tuoteryhmaModel=null;
-  
+  private $tuoteryhmaModel = null;
+  private $loginModel = null;
+
   public function __construct()
   {
-      $session = \Config\Services::session();
-      $session->start();
-      $this->tuoteryhmaModel = new TuoteRyhmaModel();
+    $this->tuoteryhmaModel = new TuoteRyhmaModel();
+    $this->loginModel = new LoginModel();
   }
-	
-	public function index()
-	{
-        $data['tuoteryhmat'] = $this->tuoteryhmaModel->haeTuoteryhmat();
-        echo view('templates/header', $data);
-		echo view('yhteystiedot.php');
-		echo view('templates/footer');
-    }
+
+  public function index()
+  {
+    $data['tuoteryhmat'] = $this->tuoteryhmaModel->haeTuoteryhmat();
+    echo view('templates/header', $data);
+    echo view('yhteystiedot.php');
+    echo view('templates/footer');
+  }
 }
