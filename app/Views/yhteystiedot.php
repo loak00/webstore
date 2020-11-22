753ec22afeda96a6bf76bf8efe1551ef90b3b96d
c9nini00 <73959407+c9nini00@users.noreply.github.com>
2020-11-22 14:51:24 +0200

    Yhteystiedot ja google maps.

--- app/Views/yhteystiedot.php
+++ app/Views/yhteystiedot.php
@@ -1,11 +1,15 @@
 <form method="post">
   <div class="row">
     <div class="col-md-6">
-      <p>tähän tullee vaikka se mapsi</p>
+      <p>Löydät meidät täältä!</p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1686.2784642669205!2d25.475534252002312!3d64.9982826839089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4681cd4b2d725cd7%3A0xfcaccd3751e3b27!2sPaljetie%2010%2C%2090150%20Oulu!5e0!3m2!1sfi!2sfi!4v1605633454684!5m2!1sfi!2sfi" width="240" height="212" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
     <div class="col-md-6">
-      <p>tähän tullee sitte meijän yhteystiedot</p>
-      <p></p>
+      <p>Ota yhteyttä!</p>
+      <p>Cippoi & Cuppei Oy <br>
+          Paljetie 10 <br>
+          90150 OULU</p>
+      <p>tel. 044 777 6655</p>
       
       
     </div>
@@ -14,12 +18,12 @@
 </form>
 <div class="row text-center">
     <div class="col-md-4">
-<i class="fab fa-facebook-square fa-4x"></i>
+<a href="https://www.facebook.com/"> <i class="fab fa-facebook-square fa-4x"></i></a>
 </div>
 <div class="col-md-4">
-<i class="fab fa-twitter-square fa-4x"></i>
+<a href="https://twitter.com/"><i class="fab fa-twitter-square fa-4x"></i></a>
 </div>
 <div class="col-md-4">
-<i class="fab fa-instagram fa-4x"></i>
+<a href="https://www.instagram.com/"><i class="fab fa-instagram fa-4x"></i></a>
 </div>
       </div>
