2f25bc60a3bcfad65937c7c5cf60445d7061d021
Aki L <n9loak00@students.oamk.fi>
2020-11-24 19:58:59 +0200

    Kauppakorttien muutosta

--- app/Views/kauppa.php
+++ app/Views/kauppa.php
@@ -1,17 +1,15 @@
  <div class="row pt-5">
      <?php foreach ($tuotteet as $tuote) : ?>
       <div class="col-lg-3 col-md-6 mb-4">
-       <div class="card h-100 testi">
+       <div class="card h-100 text-center">
          <a href="<?= site_url('kauppa/tuote/' . $tuote['id']) ?>">
            <div class="card-body">
-             <h4 class="card-title">
-               <?= $tuote['nimi'] ?>
-             </h4>
-             <h5><?= $tuote['hinta'] ?> €</h5>
+             <h5 class="card-title"><?= $tuote['nimi'] ?></h5>
+             <h5 class="card-header"><?= $tuote['hinta'] ?> €</h5>
              <img class="img-fluid" src="<?= base_url('img/' . $tuote['kuva']) ?>" alt="<?= $tuote['kuvan_kuvaus'] ?>"></img>
-             <p><?= $tuote['kuvaus'] ?></p>
+             <p class="card-footer text-muted"><?= $tuote['kuvaus'] ?></p>
              <form method="post" action="<?= site_url('ostoskori/lisaa2/' . $tuote['id']); ?>">
-               <button class="bnt btn-primary osta">Lisää koriin</button>
+               <button class="bnt btn-success">Lisää koriin</button>
              </form>
            </div>
          </a>
