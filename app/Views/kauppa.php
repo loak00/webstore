adbffb57b3900671c5d2f39d9e580a95dc5955fe
Aki L <n9loak00@students.oamk.fi>
2020-11-24 19:09:34 +0200

    Ostonappi lisätty korttiin

--- app/Views/kauppa.php
+++ app/Views/kauppa.php
@@ -1,6 +1,6 @@
  <div class="row pt-5">
-   <div class="col-lg-3 col-md-6 mb-4">
      <?php foreach ($tuotteet as $tuote) : ?>
+      <div class="col-lg-3 col-md-6 mb-4">
        <div class="card h-100 testi">
          <a href="<?= site_url('kauppa/tuote/' . $tuote['id']) ?>">
            <div class="card-body">
@@ -8,11 +8,14 @@
                <?= $tuote['nimi'] ?>
              </h4>
              <h5><?= $tuote['hinta'] ?> €</h5>
-             <img class="img-fluid" src="<?= base_url('img/' . $tuote['kuva'])?>" alt="<?=$tuote['kuvan_kuvaus']?>"></img>
+             <img class="img-fluid" src="<?= base_url('img/' . $tuote['kuva']) ?>" alt="<?= $tuote['kuvan_kuvaus'] ?>"></img>
              <p><?= $tuote['kuvaus'] ?></p>
+             <form method="post" action="<?= site_url('ostoskori/lisaa2/' . $tuote['id']); ?>">
+               <button class="bnt btn-primary osta">Lisää koriin</button>
+             </form>
            </div>
          </a>
        </div>
+       </div>
      <?php endforeach; ?>
-   </div>
  </div>
