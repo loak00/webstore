1504e061a5c9de1d9a42ad376c1dab5dbea38874
Juuso Erkkilä <n9erju000@students.oamk.fi>
2020-12-01 14:34:41 +0200

    hienosäätöä

--- app/Views/templates/header_admin.php
+++ app/Views/templates/header_admin.php
@@ -9,11 +9,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('/css/style.css');?>">
-    <title>Verkkokauppa</title>
+    <title>Cippoi & Cuppei Control Panel</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Verkkokauppa</a>
+      <a class="navbar-brand" href="#">Cippoi & Cuppei Control Panel</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
