1c2e11f704fe7c50fdc5c586f0e54668e24ae6b5
Aki L <n9loak00@students.oamk.fi>
2020-12-04 16:06:45 +0200

    Korttisäätöä

--- app/Views/kauppa.php
+++ app/Views/kauppa.php
@@ -5,9 +5,9 @@
          <a href="<?= site_url('kauppa/tuote/' . $tuote['id']) ?>">
            <div class="card-body shadow p-3">
              <h5 class="card-title"><?= $tuote['nimi'] ?></h5>
-             <h5 class="card-header"><?= $tuote['hinta'] ?> €</h5>
+             <h4><?= $tuote['hinta'] ?> €</h4>
              <img class="img-fluid" src="<?= base_url('img/thumb_' . $tuote['kuva']) ?>" alt="<?= $tuote['kuvan_kuvaus'] ?>"></img>
-             <p class="card-footer text-muted"><?= $tuote['kuvaus'] ?></p>
+             <p class="text-muted korttifooter"><?= $tuote['kuvaus'] ?></p>
              <form method="post" action="<?= site_url('ostoskori/lisaa/' . $tuote['id'] . "/" . $tuote['tuoteryhma_id']); ?>">
                <button class="bnt btn-primary shadow rounded"><i class="fas fa-shopping-basket"></i> &nbsp Lisää ostokoriin</button>
              </form>
