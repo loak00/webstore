88fbd59ecf226f62b0b64503d2e50a1fb99673d2
Aki L <n9loak00@students.oamk.fi>
2020-12-05 13:51:43 +0200

    Tuotesivun säätöä

--- app/Views/tuote.php
+++ app/Views/tuote.php
@@ -1,9 +1,9 @@
 <form method="post" action="<?= site_url('ostoskori/lisaa/' . $tuote['id']); ?>">
   <div class="row">
     <div class="col-md-6">
-      <img class="img-fluid" src="<?= base_url('img/' . $tuote['kuva']) ?>"  alt="<?=$tuote['kuvan_kuvaus']?>">
+      <img class="img-fluid tuotekuva float-right" src="<?= base_url('img/' . $tuote['kuva']) ?>"  alt="<?=$tuote['kuvan_kuvaus']?>">
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 text-center">
       <p><?= $tuote['nimi']; ?></p>
       <p><?= $tuote['kuvaus']; ?></p>
       <p class="hinta"><?= $tuote['hinta']; ?> €</p>
