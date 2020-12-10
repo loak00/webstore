d5a39185fa81e6219a0cccaba312201b115a4fe6
Aki L <n9loak00@students.oamk.fi>
2020-12-10 16:13:39 +0200

    Uusi tuotesivu

--- app/Views/tuote.php
+++ app/Views/tuote.php
@@ -1,13 +1,15 @@
 <form method="post" action="<?= site_url('ostoskori/lisaa/' . $tuote['id']); ?>">
-  <div class="row">
-    <div class="col-md-6">
-      <img class="img-fluid tuotekuva float-right" src="<?= base_url('img/' . $tuote['kuva']) ?>"  alt="<?=$tuote['kuvan_kuvaus']?>">
+  <div class="row mt-5">
+    <div class="col-md-6 d-flex align-items-center justify-content-center">
+      <img class="img-fluid tuotekuva shadow-lg rounded mb-3" src="<?= base_url('img/' . $tuote['kuva']) ?>"  alt="<?=$tuote['kuvan_kuvaus']?>">
     </div>
     <div class="col-md-4 text-center">
-      <p><?= $tuote['nimi']; ?></p>
-      <p><?= $tuote['kuvaus']; ?></p>
+    <img class="img-fluid mb-2" src="<?= base_url('img/tuote-top.png') ?>" alt="Ylä-ornamentti">
+      <h1 class="tuotelogo"><?= $tuote['nimi']; ?></h1>
+      <p class="tuoteteksti"><?= $tuote['kuvaus']; ?></p>
       <p class="hinta"><?= $tuote['hinta']; ?> €</p>
-      <button class="bnt btn-primary shadow rounded osta">Osta</button>
+      <button class="bnt btn-primary shadow rounded"><i class="fas fa-shopping-basket"></i> &nbsp Lisää koriin</button>
+      <img class="img-fluid mt-3" src="<?= base_url('img/tuote-bottom.png') ?>" alt="Ala-ornamentti">
     </div>
   </div>
 </form>
