850a40f900c076f9fb37be910c9e553143f17ef9
marttilahelina <n9mama01@students.oamk.fi>
2020-12-14 20:20:48 +0200

    Siivottu kiitos tilauksesta-sivua

--- app/Views/kiitos.php
+++ app/Views/kiitos.php
@@ -1,1 +1,26 @@
-<h4>Kiitos tilauksesta</h4>
+<div class="row text-center">
+    <div class="col-md-8">
+        <p>
+            <h2>Kiitos tilauksestasi!</h2>
+        </p>
+        <span>
+            <a class="nav-link" href="<?= site_url('home/index'); ?>">
+                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                Takaisin verkkokauppaan</a>
+        </span>
+    </div>
+    <div class="col-md-4 text-center">
+        <form method="post" action="<?= site_url('yhteystiedot/uutiskirje/'); ?>">
+            <div class="container">
+                <p>
+                    <h4>Tilaa uutiskirje</h4>
+                </p>
+                <p>Saat sähköpostiisi kuukausittain postin uutuustuotteistamme ja verrattomista erikoisalennuksista! Täyttämällä yhteystietosi
+                    ja tilaamalla uutiskirjeen saat etuna 10% alennukseen oikeuttavan alennuskoodin! Tarkemmat ohjeet saat sähköpostiisi!</p>
+            </div>
+            <div class="container">
+                <input type="email" placeholder="Sähköpostiosoitteesi" name="mail" required>
+                <button class="bnt btn-primary tilaa">Tilaa tästä</button>
+            </div>
+        </form>
+    </div>
