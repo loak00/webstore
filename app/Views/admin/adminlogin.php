27a6f3b61e0ec17aa492866deb9b4f883d5c8e51
c9nini00 <73959407+c9nini00@users.noreply.github.com>
2020-12-15 18:42:25 +0200

    Admin kirjautumiseen kuva ja vari.

--- app/Views/admin/adminlogin.php
+++ app/Views/admin/adminlogin.php
@@ -1,10 +1,14 @@
-<h3>Admin kirjautuminen</h3>
-<form action="/admin/check">
+<div style="text-align:center;background-color:lightpink; padding:50px;">
+   <img src="/adminimg/adminnerd.png" alt="nerd kuva" width="200"
+         height="200">
+
+    <h3>Admin kirjautuminen</h3>
+    <form action="/admin/check">
     <div class="col-12">
     <?= \Config\Services::validation()->listErrors(); ?>
     </div>
     <div class="form-group">
-        <label>Adminname</label>
+        <label>Admin name</label>
         <input
         class="form-control" 
         name="adminname" 
@@ -19,7 +23,9 @@
         type="password" 
         placeholder="Enter password" 
         maxlength="30">
-    </div>
-    <button class="btn btn-primary">Login</button>
-    
-</form>
+    </div><br>
+    <button class="btn btn-primary">Login</button><br>
+    </form>
+</div>
+
+
